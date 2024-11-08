19c2481f5549f2cd0a02bc3d9cc38abbee48fcdd
Alex Wu <108106616+Alexwu0706@users.noreply.github.com>
2024-11-08 11:20:19 -0800

    update

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
 const controls = new OrbitControls(camera, renderer.domElement);
-camera.position.set(0, 10, 5); 
+camera.position.set(0, 10, 2); 
 controls.target.set(0, 5, 0);
 
 // Rendering 3D axis
@@ -152,7 +152,6 @@ custom_cube_geometry.setAttribute('position', new THREE.BufferAttribute(position
 custom_cube_geometry.setAttribute('normal', new THREE.BufferAttribute(normals, 3));
 custom_cube_geometry.setIndex(new THREE.BufferAttribute(new Uint16Array(indices), 1));
 
-///secion 3////////////////////////////////////////////////////////////////
 function translationMatrix(tx, ty, tz) {
 	return new THREE.Matrix4().set(
 		1, 0, 0, tx,
@@ -179,6 +178,7 @@ function scalingMatrix(sx, sy, sz) {
     0,   0,   0,   1,
   );
 }
+//----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 ///Initialization////////////////////////////////////////////////////////////////
 const wall_material = new THREE.MeshPhongMaterial({
@@ -191,6 +191,10 @@ shininess: 100
 });
 const box_material = new THREE.MeshPhongMaterial({
   color: 0xffff00, // Yellow color
+  shininess: 100   
+});
+const ground_material = new THREE.MeshPhongMaterial({
+  color: 0xffffff, // White color
   shininess: 100   
 });
 
@@ -218,6 +222,7 @@ function onKeyPress(event) {
             break;     
         case 'r':
             resetM = true;
+            console.log("resetM set to true");  // Debugging line
             break;
         default:
             console.log(`Key ${event.key} pressed`);
@@ -230,30 +235,39 @@ let Wx_1 = [1,2,3,3,3,2,1,0,0,0,-1,-2,-2,-2,-2,-3,-4,-4,-4,-3,-2,-1,-1,-1,0,1,1,
 let Wz_1 = [0,0,0,1,2,2,2,2,3,4,4,4,3,2,1,1,1,0,-1,-1,-1,-1,-2,-3,-3,-3,-2,-1];
 let Bx_1 = [0,-1,-1,1];
 let Bz_1 = [-1,0,1,1];
-let Blx_1 = [-3,0,2,-1];
-let Blz_1 = [0,-2,1,3];
+let Gx_1 = [0,-2,-1,0,-1,0,1,2,-1];
+let Gz_1 = [-1,0,0,0,1,1,1,1,2];
+let Btx_1 = [-3,0,2,-1];
+let Btz_1 = [0,-2,1,3];
 
 //Map2
 let Wx_2 = [0,0,-1,-2,-3,-3,-3,-3,-3,-2,-2,-1,0,0,0,1,2,2,2,3,4,4,4,4,3,2,1,1];
 let Wz_2 = [-1,-2,-2,-2,-2,-1,0,1,2,2,3,3,3,4,5,5,5,4,3,3,3,2,1,0,0,0,0,-1];
 let Bx_2 = [0,2,-1];
 let Bz_2 = [2,1,1];
-let Blx_2 = [-2,-1,1];
-let Blz_2 = [0,2,3];
+let Gx_2 = [-2,-1,-1,0,-2,-1,0,1,2,3,0,1,2,3,1];
+let Gz_2 = [-1,-1,0,0,1,1,1,1,1,1,2,2,2,2,4];
+let Btx_2 = [-2,-1,1];
+let Btz_2 = [0,2,3];
 
 //Map3 (Undefined)
-let Wx_3 = [1,2,1,2,-3,-3,-3,-3,-3,-2,-2,-1,0,0,0,1,2,2,2,3,4,4,4,4,3,2,1,1];
-let Wz_3 = [1,2,2,2,-2,-1,0,1,2,2,3,3,3,4,5,5,5,4,3,3,3,2,1,0,0,0,0,-1];
-let Bx_3 = [0,2,-1];
-let Bz_3 = [2,1,1];
-let Blx_3 = [-2,-1,1];
-let Blz_3 = [0,2,3];
+let Wx_3 = [1,1,1,1,0,-1,-2,-3,-4,-4,-5,-6,-6,-6,-6,-5,-4,-4,-3,-2,-2,-1,0,1,1,1,1];
+let Wz_3 = [0,1,2,3,3,3,3,2,2,1,1,1,0,-1,-2,-2,-2,-3,-3,-3,-4,-4,-4,-4,-3,-2,-1];
+let Bx_3 = [-1,-1,-2,-3];
+let Bz_3 = [-1,-2,0,-1];
+let Gx_3 = [-2,-1,0,-3,-2,-1,0,-4,-3,-2,-1,0,-3,-2,-1,0,-2,-1,0,-1,0];
+let Gz_3 = [2,2,2,1,1,1,1,0,0,0,0,0,-1,-1,-1,-1,-2,-2,-2,-3,-3];
+let Btx_3 = [-5,-5,-4,-3];
+let Btz_3 = [0,-1,-1,-2];
+
+//Map4
+//Map5
 
 ///animation////////////////////////////////////////////////////////////////
 let animation_time = 0;
 let delta_animation_time;
 const clock = new THREE.Clock();
-let flag = 1; //Map Update
+let flag = 3; //Map Update
 
 let players = [];
 for (let i = 0; i < 1; i++) {
@@ -267,60 +281,76 @@ function animate() {
   let Wz = [];
   let Bx = [];
   let Bz = [];
-  let Blx = [];
-  let Blz = [];
+  let Btx = [];
+  let Btz = [];
+  let Gx = [];
+  let Gz = [];
   let wall = [];
   let walls = [];
   let box = [];
   let boxes = [];
-  let box_location = [];
-  let boxes_location = [];
+  let box_target = [];
+  let boxes_target = [];
+  let ground = [];
+  let grounds = [];
 
 	renderer.render( scene, camera );
     controls.update();
     // delta_animation_time = clock.getDelta();
     // animation_time += delta_animation_time; 
     
-    if (flag > 2) flag = 1;
+    if (flag > 3) flag = 1;
     if (flag == 1){
       Wx = Wx_1;
       Wz = Wz_1;
       Bx = Bx_1;
       Bz = Bz_1;
-      Blx = Blx_1;
-      Blz = Blz_1;
+      Btx = Btx_1;
+      Btz = Btz_1;
+      Gx = Gx_1;
+      Gz = Gz_1;
     }else if(flag == 2){
       Wx = Wx_2;
       Wz = Wz_2;
       Bx = Bx_2;
       Bz = Bz_2;
-      Blx = Blx_2;
-      Blz = Blz_2;
+      Btx = Btx_2;
+      Btz = Btz_2;
+      Gx = Gx_2;
+      Gz = Gz_2;
     }else if(flag == 3){
       Wx = Wx_3;
       Wz = Wz_3;
       Bx = Bx_3;
       Bz = Bz_3;
-      Blx = Blx_3;
-      Blz = Blz_3;
+      Btx = Btx_3;
+      Btz = Btz_3;
+      Gx = Gx_3;
+      Gz = Gz_3;
     }
 
     //Initialization
     for (let i = 0; i < Wx.length; i++) {
-      wall = new THREE.Mesh(custom_cube_geometry, wall_material);     //geometry and material is adjustable (refer to assignment 3)
+      wall = new THREE.Mesh(custom_cube_geometry, wall_material);     //Todo: geometry and material are adjustable (refer to assignment 3)
       wall.matrixAutoUpdate = false;
       walls.push(wall);
       scene.add(wall);      
     }
     for (let i = 0; i < Bx.length; i++) {
       box = new THREE.Mesh(custom_cube_geometry, box_material);
-      box_location = new THREE.Mesh(custom_cube_geometry, box_material);
+      box_target = new THREE.Mesh(custom_cube_geometry, box_material);
       box.matrixAutoUpdate = false;
-      box_location.matrixAutoUpdate = false;
+      box_target.matrixAutoUpdate = false;
       boxes.push(box);
-      boxes_location.push(box_location);
+      boxes_target.push(box_target);
       scene.add(box);
-      scene.add(box_location);
+      scene.add(box_target);
+    }
+    for (let i=0; i< Gx.length; i++){
+      ground = new THREE.Mesh(custom_cube_geometry, ground_material);
+      ground.matrixAutoUpdate = false;
+      grounds.push(ground);
+      scene.add(ground);
     }
 
     //Transformation
@@ -333,9 +363,14 @@ function animate() {
       let Transform_Box = new THREE.Matrix4();
       let Transform_BoxL = new THREE.Matrix4();
       Transform_Box.multiply(translationMatrix(Bx[i],0,Bz[i]));
-      Transform_BoxL.multiply(translationMatrix(Blx[i],-l,Blz[i])).multiply(scalingMatrix(1,1/100,1));
+      Transform_BoxL.multiply(translationMatrix(Btx[i],-l,Btz[i])).multiply(scalingMatrix(1,1/50,1));
       boxes[i].matrix.copy(Transform_Box);
-      boxes_location[i].matrix.copy(Transform_BoxL);
+      boxes_target[i].matrix.copy(Transform_BoxL);
+    }
+    for (let i=0; i< Gx.length; i++){
+      let Transform_Ground = new THREE.Matrix4();
+      Transform_Ground.multiply(translationMatrix(Gx[i],-l,Gz[i])).multiply(scalingMatrix(1,1/1000,1));
+      grounds[i].matrix.copy(Transform_Ground);
     }
 
     //Player Motion
@@ -354,39 +389,51 @@ function animate() {
     }
     
     //Reset
-    // walls.forEach(wall => scene.remove(wall));
-    // walls = []; // Clear the walls array
-    
-    // // Remove boxes
-    // boxes.forEach(box => scene.remove(box));
-    // boxes = []; // Clear the boxes array
-    
-    // // Remove box locations
-    // boxes_location.forEach(boxLocation => scene.remove(boxLocation));
-    // boxes_location = []; // Clear the box locations array
-    if (resetM) {
-      console.log("yes")
-      walls.forEach(wall => scene.remove(wall));
-      walls = []; // Clear the walls array
-      
-      // Remove boxes
-      boxes.forEach(box => scene.remove(box));
-      boxes = []; // Clear the boxes array
-      
-      // Remove box locations
-      boxes_location.forEach(boxLocation => scene.remove(boxLocation));
-      boxes_location = []; // Clear the box locations array
-      // Reset flag for next map
-      flag = flag + 1;
-      resetM = false;
-    }
 
+    // if(true){
+    //   for (let i = 0; i < Wx.length; i++) {
+    //     scene.remove(walls[i]);      
+    //   }
+    //   for (let i = 0; i < Bx.length; i++) {
+    //     scene.remove(boxes[i]);
+    //     scene.remove(boxes_target[i]);
+    //   }
+    // }
+    
+    if(resetM){                                       //why does my removal not working properly even if my if statement gets triggered?
+      console.log("Running reset");
+      for (let i = 0; i < Wx.length; i++) {
+        scene.remove(walls[i]);      
+      }
+      for (let i = 0; i < Bx.length; i++) {
+        scene.remove(boxes[i]);
+        scene.remove(boxes_target[i]);
+      }
+      //resetM = false;
+    }
+    
+
+
+
+
+
+
+
+
+
+
+
+    // if (resetM === true) {
+    //   // Reset flag for next map
+    //   //flag = flag + 1;
+    //   //resetM = false;
+    // }
 
     //Condition for Reset Maps ()
         
-    
     //Interaction Implementation
-        //console.log((walls[4].matrix)) (Position)
+        //console.log((walls[4].matrix)) (Position)         //what condition should I use if I want to know whether my box location is overlapping with its targeted location?
+                                                            //e.q by saying like if their matrix is equal? 
 
 }
 renderer.setAnimationLoop( animate );
